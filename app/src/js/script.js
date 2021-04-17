c9aa09ec5cbf997b33e9ccf3b2a2321c364378fb
shoffkarim <k4r1mch1k@gmail.com>
2021-04-17 16:40:23 +0500

    футер

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -5,6 +5,7 @@ import '../sass/style.sass';
 
 const swiper = new Swiper('.instagram-slider', {
   slidesPerView: 4,
+  spaceBetween: 32,
   navigation: {
     nextEl: '.slider-next-btn',
     prevEl: '.slider-prev-btn',
