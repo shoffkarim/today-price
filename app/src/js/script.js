98cb172376d02e1fe150b4552b13edd90f16f72b
shoffkarim <k4r1mch1k@gmail.com>
2021-04-17 15:06:34 +0500

    блок со слайдером инстаграмма

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,2 +1,12 @@
-// STYLES=============================================================================
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
 import '../sass/style.sass';
+
+const swiper = new Swiper('.instagram-slider', {
+  slidesPerView: 4,
+  navigation: {
+    nextEl: '.slider-next-btn',
+    prevEl: '.slider-prev-btn',
+  },
+});
